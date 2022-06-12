a5dd847a41337940751fc9592e41c554688d87c6
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-12 20:19:57 +0100

    first commit

---
+++ src/formData.js
@@ -0,0 +1,54 @@
+export const userInputs = [
+  {
+    id: 1,
+    type: 'text',
+    name: 'firstName',
+    label: 'First Name',
+    placeholder: 'First Name',
+  },
+  {
+    id: 2,
+    type: 'text',
+    name: 'lastName',
+    label: 'Last Name',
+    placeholder: 'Last Name',
+  },
+  {
+    id: 3,
+    type: 'email',
+    name: 'email',
+    label: 'Email',
+    placeholder: 'Email',
+  },
+  {
+    id: 4,
+    type: 'password',
+    name: 'password',
+    label: 'Password',
+    placeholder: 'Password',
+  },
+  {
+    id: 5,
+    type: 'password',
+    name: 'passwordConfirm',
+    label: 'Confirm Password',
+    placeholder: 'Confirm Password',
+  },
+];
+
+export const tourInputs = [
+  {
+    id: 1,
+    type: 'text',
+    name: 'title',
+    label: 'Title',
+    placeholder: 'Title',
+  },
+  {
+    id: 2,
+    type: 'text',
+    name: 'description',
+    label: 'Description',
+    placeholder: 'Description',
+  },
+];
